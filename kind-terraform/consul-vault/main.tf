ee92bdd892bf430a411d1d1f0aa6e44861f0e951
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2023-03-12 14:03:05 -0400

    vault dev mode

--- kind-terraform/consul-vault/main.tf
+++ kind-terraform/consul-vault/main.tf
@@ -151,14 +151,14 @@ resource "helm_release" "vault" {
   ]
 }
 
-locals {
-  crds_split_doc  = split("---", file("${path.module}/vault-ingress.yaml"))
-  crds_valid_yaml = [for doc in local.crds_split_doc : doc if try(yamldecode(doc).metadata.name, "") != ""]
-  crds_dict       = { for doc in toset(local.crds_valid_yaml) : yamldecode(doc).metadata.name => doc }
-}
+# locals {
+#   crds_split_doc  = split("---", file("${path.module}/vault-ingress.yaml"))
+#   crds_valid_yaml = [for doc in local.crds_split_doc : doc if try(yamldecode(doc).metadata.name, "") != ""]
+#   crds_dict       = { for doc in toset(local.crds_valid_yaml) : yamldecode(doc).metadata.name => doc }
+# }
 
-resource "kubectl_manifest" "crds" {
-  for_each  = local.crds_dict
-  yaml_body = each.value
-  depends_on = [ helm_release.vault ]
-}
+# resource "kubectl_manifest" "crds" {
+#   for_each  = local.crds_dict
+#   yaml_body = each.value
+#   depends_on = [ helm_release.vault ]
+# }
